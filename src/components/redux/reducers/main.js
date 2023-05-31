ecb1e4b96406bf104ff5d945ca82de384551fad2
Bassel196 <125680928+Bassel196@users.noreply.github.com>
2023-05-31 05:44:48 +0300

    file arrangement

--- src/components/redux/reducers/main.js
+++
@@ -1,9 +0,0 @@
-import { getProductsreducer } from "./Productreducer"
-
-import {combineReducers} from "redux"
-
-const rootreducers = combineReducers({
-    getproductsdata : getProductsreducer
-})
-
-export default rootreducers
